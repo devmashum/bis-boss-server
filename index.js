e9c6cfb6d98052715de739f25fe2f03e892112b4
devmashum <dev.mashum@gmail.com>
2023-11-23 01:00:12 +0100

    created payment intent with stripe

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -158,6 +161,14 @@ async function run() {
             res.send(result);
         })
 
+        // Update Items
+        app.get('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.findOne(query);
+            res.send(result);
+        })
+
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result);
@@ -170,6 +181,23 @@ async function run() {
                 expiresIn: '1h'
             });
             res.send({ token });
+        })
+        // Payment Intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: [
+                    "card"
+                ],
+
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
         })
 
 
